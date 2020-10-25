4629f71a8b250c4d37761bb7cd6242eb231f4d06
phantomVK <phantomvk@163.com>
2020-10-25 08:40:50 +0800

    Thread priority.

--- library/src/main/java/com/phantomvk/dag/library/meta/AsyncTask.java
+++ library/src/main/java/com/phantomvk/dag/library/meta/AsyncTask.java
@@ -9,6 +9,6 @@ public abstract class AsyncTask extends Task {
 
     @Override
     public int getPriority() {
-        return Process.THREAD_PRIORITY_LOWEST;
+        return Process.THREAD_PRIORITY_BACKGROUND;
     }
 }
